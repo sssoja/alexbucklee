bd926c02a965b81173f363ee016dd8f7c043cb45
sophie <sophieleila@icloud.com>
2020-08-23 19:29:33 +0200

    adds custom schema to desk structure

--- studio/deskStructure.js
+++ studio/deskStructure.js
@@ -1,36 +1,46 @@
-import S from '@sanity/desk-tool/structure-builder'
-import MdSettings from 'react-icons/lib/md/settings'
+import S from "@sanity/desk-tool/structure-builder";
+import MdSettings from "react-icons/lib/md/settings";
 
 const hiddenDocTypes = listItem =>
-  !['category', 'person', 'sampleProject', 'siteSettings'].includes(listItem.getId())
+  !["category", "person", "sampleProject", "siteSettings", "art", "design"].includes(
+    listItem.getId()
+  );
 
 export default () =>
   S.list()
-    .title('Content')
+    .title("Content")
     .items([
       S.listItem()
-        .title('Settings')
+        .title("Settings")
         .child(
           S.editor()
-            .id('siteSettings')
-            .schemaType('siteSettings')
-            .documentId('siteSettings')
+            .id("siteSettings")
+            .schemaType("siteSettings")
+            .documentId("siteSettings")
         )
         .icon(MdSettings),
       S.listItem()
-        .title('Sample projects')
-        .schemaType('sampleProject')
-        .child(S.documentTypeList('sampleProject').title('Sample projects')),
+        .title("Sample projects")
+        .schemaType("sampleProject")
+        .child(S.documentTypeList("sampleProject").title("Sample projects")),
       S.listItem()
-        .title('People')
-        .schemaType('person')
-        .child(S.documentTypeList('person').title('People')),
+        .title("People")
+        .schemaType("person")
+        .child(S.documentTypeList("person").title("People")),
       S.listItem()
-        .title('Categories')
-        .schemaType('category')
-        .child(S.documentTypeList('category').title('Categories')),
+        .title("Categories")
+        .schemaType("category")
+        .child(S.documentTypeList("category").title("Categories")),
+      S.listItem()
+        .title("Art work")
+        .schemaType("art")
+        .child(S.documentTypeList("art").title("Art work")),
+      S.listItem()
+        .title("Design work")
+        .schemaType("design")
+        .child(S.documentTypeList("design").title("Design work")),
       // This returns an array of all the document types
       // defined in schema.js. We filter out those that we have
       // defined the structure above
       ...S.documentTypeListItems().filter(hiddenDocTypes)
-    ])
+    ]);
