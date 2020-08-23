88a535ab9e26a073c5cb15a05bb28036b8bd09d9
sophie <sophieleila@icloud.com>
2020-08-23 19:28:11 +0200

    creates studio schema for art projects

--- studio/schemas/schema.js
+++ studio/schemas/schema.js
@@ -1,26 +1,28 @@
 // First, we must import the schema creator
-import createSchema from 'part:@sanity/base/schema-creator'
+import createSchema from "part:@sanity/base/schema-creator";
 
 // Then import schema types from any plugins that might expose them
-import schemaTypes from 'all:part:@sanity/base/schema-type'
+import schemaTypes from "all:part:@sanity/base/schema-type";
 
 // Document types
-import category from './documents/category'
-import person from './documents/person'
-import sampleProject from './documents/sampleProject'
-import siteSettings from './documents/siteSettings'
+import category from "./documents/category";
+import person from "./documents/person";
+import art from "./documents/art";
+import design from "./documents/design";
+import sampleProject from "./documents/sampleProject";
+import siteSettings from "./documents/siteSettings";
 
 // Object types
-import bioPortableText from './objects/bioPortableText'
-import figure from './objects/figure'
-import projectMember from './objects/projectMember'
-import projectPortableText from './objects/projectPortableText'
-import simplePortableText from './objects/simplePortableText'
+import bioPortableText from "./objects/bioPortableText";
+import figure from "./objects/figure";
+import projectMember from "./objects/projectMember";
+import projectPortableText from "./objects/projectPortableText";
+import simplePortableText from "./objects/simplePortableText";
 
 // Then we give our schema to the builder and provide the result to Sanity
 export default createSchema({
   // We name our schema
-  name: 'portfolio',
+  name: "portfolio",
   // Then proceed to concatenate our our document type
   // to the ones provided by any plugins that are installed
   types: schemaTypes.concat([
@@ -33,9 +35,11 @@ export default createSchema({
     simplePortableText,
     // The following are document types which will appear
     // in the studio.
+    art,
     category,
+    design,
     person,
     sampleProject,
     siteSettings
   ])
-})
+});
