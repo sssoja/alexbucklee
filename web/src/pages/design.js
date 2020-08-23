6c1286126f72e528abdadb0d127eeee06f9dda65
sophie <sophieleila@icloud.com>
2020-08-23 23:10:16 +0200

    maps data to design project nodes

--- web/src/pages/design.js
+++ web/src/pages/design.js
@@ -1,10 +1,67 @@
 import React from "react";
+import { graphql } from "gatsby";
+import {
+  mapEdgesToNodes,
+  filterOutDocsWithoutSlugs,
+  filterOutDocsPublishedInTheFuture
+} from "../lib/helpers";
 import Container from "../components/container";
 import GraphQLErrorList from "../components/graphql-error-list";
+import ProjectPreviewGrid from "../components/project-preview-grid";
+import SEO from "../components/seo";
 import Layout from "../containers/layout";
 
-const DesignWorkPage = props => {
-  const { errors } = props;
+export const query = graphql`
+  query DesignPageQuery {
+    site: sanitySiteSettings(_id: { regex: "/(drafts.|)siteSettings/" }) {
+      title
+      description
+      keywords
+    }
+    projects: allSanityDesign(
+      limit: 4
+      sort: { fields: [publishedAt], order: DESC }
+      filter: { slug: { current: { ne: null } }, publishedAt: { ne: null } }
+    ) {
+      edges {
+        node {
+          id
+          mainImage {
+            crop {
+              _key
+              _type
+              top
+              bottom
+              left
+              right
+            }
+            hotspot {
+              _key
+              _type
+              x
+              y
+              height
+              width
+            }
+            asset {
+              _id
+            }
+            alt
+          }
+          title
+          _rawExcerpt
+          slug {
+            current
+          }
+        }
+      }
+    }
+  }
+`;
+
+const DesignworkPage = props => {
+  const { data, errors } = props;
+
   if (errors) {
     return (
       <Layout>
@@ -13,13 +70,33 @@ const DesignWorkPage = props => {
     );
   }
 
+  const site = (data || {}).site;
+  const projectNodes = (data || {}).projects
+    ? mapEdgesToNodes(data.projects)
+        .filter(filterOutDocsWithoutSlugs)
+        .filter(filterOutDocsPublishedInTheFuture)
+    : [];
+
+  if (!site) {
+    throw new Error(
+      'Missing "Site settings". Open the studio at http://localhost:3333 and add some content to "Site settings" and restart the development server.'
+    );
+  }
+
   return (
     <Layout>
+      <SEO title={site.title} description={site.description} keywords={site.keywords} />
       <Container>
-        <h1>Design Works</h1>
+        {projectNodes && (
+          <ProjectPreviewGrid
+            title="Design works"
+            nodes={projectNodes}
+            browseMoreHref="/archive/"
+          />
+        )}
       </Container>
     </Layout>
   );
 };
 
-export default DesignWorkPage;
+export default DesignworkPage;
