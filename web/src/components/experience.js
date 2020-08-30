3095e8bfe7d1a33f4895fbc4027ae20f0ed0335d
sophie <sophieleila@icloud.com>
2020-08-30 20:30:38 +0200

    adds styles to mapped list items

--- web/src/components/experience.js
+++ web/src/components/experience.js
@@ -16,6 +16,24 @@ const Column = styled.div`
   ${flexbox};
 `;
 
+const List = styled.ul`
+  display: flex;
+  ${typography};
+  ${layout};
+  ${flexbox};
+  ${grid};
+`;
+
+const ListItem = styled.li`
+  ${layout};
+  ${space};
+  ${flexbox};
+`;
+
+const Paragraph = styled.p`
+  ${grid};
+`;
+
 function Experience(props) {
   const fontSizes = [0, 1, 2, 3];
   return (
@@ -31,17 +49,25 @@ function Experience(props) {
       flexDirection="column"
     >
       <h1>Bio</h1>
+      <Paragraph gridColumn="2/4">
+        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has
+        been the industry's standard dummy text ever since the 1500s, when an unknown printer took a
+        galley of type and scrambled it to make a type specimen book. It has survived not only five
+        centuries, but also the leap into electronic typesetting, remaining essentially unchanged.
+        It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum
+        passages, and more recently with desktop publishing software like Aldus PageMaker including
+        versions of Lorem Ipsum.
+      </Paragraph>
       <h1>Experience</h1>
       <Column flexDirection="column">
-        {" "}
-        <ul>
+        <List flexDirection={["column", "row"]} textAlign="left">
           {props.nodes &&
             props.nodes.map(node => (
-              <li key={node.id}>
+              <ListItem key={node.id} minWidth="fit-content" mr={6} mb={4}>
                 <Job {...node} />
-              </li>
+              </ListItem>
             ))}
-        </ul>{" "}
+        </List>
       </Column>
     </Grid>
   );
