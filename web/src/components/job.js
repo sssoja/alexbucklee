7c6631b46033e11027fc6dc410b59cadb0e34ca5
sophie <sophieleila@icloud.com>
2020-08-30 21:25:33 +0200

    fixes spacing on job post

--- web/src/components/job.js
+++ web/src/components/job.js
@@ -1,14 +1,22 @@
 import React, { Fragment } from "react";
+import styled from "styled-components";
+import { space } from "styled-system";
+
+const Div = styled.div`
+  ${space};
+`;
 
 function Job(props) {
   return (
     <Fragment>
       <div>
-        {props.company && <p>{props.company}</p>}
-        {props.role && <p>{props.role}</p>}
-        {props.date && <p>{props.date}</p>}
-        <p>Clients worked on include:</p>
+        <Div mb={2}>
+          {props.company && <p>{props.company}</p>}
+          {props.role && <p>{props.role}</p>}
+          {props.date && <p>{props.date}</p>}
 
+          <p>Clients worked on include:</p>
+        </Div>
         <ul>
           {props.clients &&
             props.clients.map(client => (
