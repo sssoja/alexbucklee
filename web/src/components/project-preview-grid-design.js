ff65f49d7773ad11bf0d71ce5cf908d68662a900
sophie <sophieleila@icloud.com>
2020-09-03 18:26:10 +0200

    removes excess spacing props

--- web/src/components/project-preview-grid-design.js
+++ web/src/components/project-preview-grid-design.js
@@ -29,17 +29,17 @@ const Row = styled.div`
 function ProjectPreviewGridDesign(props) {
   return (
     <GridWrapper
-      gridTemplateColumns="20%"
+      gridTemplateColumns="15%"
       gridTemplateRows="90%"
       display={["flex", "grid"]}
       flexDirection="column"
       fontSize={[2, 3]}
     >
-      <Column gridColumn={1} display="flex" flexDirection="column" mr={4} mb={4} mt={4}>
+      <Column gridColumn={1} display="flex" flexDirection="column" mt={4} mr={4}>
         {props.title && <h2>{props.title}</h2>}
       </Column>
 
-      <Row gridColumn={2} pb={4} className={styles.scroll} mt={4}>
+      <Row gridColumn={2} className={styles.scroll} mt={[2, 4]}>
         <Column className={styles.grid}>
           <Column className={styles.grid} fontSize={5}>
             <ul>
