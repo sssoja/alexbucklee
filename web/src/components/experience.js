a438b515300f1569d4b4ae2f9b4a8c034bf36d9d
sophie <sophieleila@icloud.com>
2020-09-05 23:40:01 +0200

    adjusts styling to fix display bug

--- web/src/components/experience.js
+++ web/src/components/experience.js
@@ -5,24 +5,28 @@ import styles from "./about.module.css";
 function Experience(props) {
   return (
     <div className={styles.wrapper}>
-      <div className={styles.bio}>
-        <h1 className={styles.h1}>Bio</h1>
-        <div className={styles.verticalDiv1}></div>
-        <div> {props.bio && <p>{props.bio}</p>}</div>
+      <div className={styles.container1}>
+        <div className={styles.row}>
+          <h1 className={styles.h1}>Bio</h1>
+          <div className={styles.verticalDiv1}></div>
+          <div> {props.bio && <p>{props.bio}</p>}</div>
+        </div>
       </div>
 
-      <div className={styles.row}>
-        <h1 className={styles.h1}>Experience</h1>
-        <div className={styles.verticalDiv2}></div>
+      <div className={styles.container2}>
         <div className={styles.row}>
-          <ul className={styles.list}>
-            {props.nodes &&
-              props.nodes.map(node => (
-                <li key={node.id} className={styles.listItem}>
-                  <Job {...node} />
-                </li>
-              ))}
-          </ul>
+          <h1 className={styles.h1}>Experience</h1>
+          <div className={styles.verticalDiv2}></div>
+          <div className={styles.row}>
+            <ul className={styles.list}>
+              {props.nodes &&
+                props.nodes.map(node => (
+                  <li key={node.id} className={styles.listItem}>
+                    <Job {...node} />
+                  </li>
+                ))}
+            </ul>
+          </div>
         </div>
       </div>
     </div>
